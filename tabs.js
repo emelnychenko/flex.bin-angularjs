7a7133deac801288cde6eeab62c38dfe2ef56f95
Eugen Melnychenko <eugen.melnychenko@gmail.com>
2017-01-18 12:50:49 +0200

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,61 +1,66 @@
 (function() {
     'use strict';
 
-    var $bin = angular.module('flex.bin', []);
+    var $bin = angular.module('flex.bin.tab', []);
 
     $bin.service('fx-tab', ['$window', function($window) {
-        var $self = this;
-        var $hash = location.hash.replace(/^(\#\/|\#)/i, '');
-        var $tabs = new Object;
-        var $show = new Object;
+        var self = this;
+        var hash = location.hash.replace(/^(\#\/|\#)/i, '');
+        var tabs = new Object;
+        var show = new Object;
 
-        this.add = function($space, $name) {
-            if (typeof $tabs[$space] === 'undefined')
-                $tabs[$space] = new Array;
+        self.add = function(space, name) {
+            if (typeof tabs[space] === 'undefined')
+                tabs[space] = new Array;
 
-            $tabs[$space].push($name);
+            tabs[space].push(name);
         };
 
-        this.toggle = function($space, $name) {
-            var $element;
+        self.toggle = function(space, name) {
+            var element;
 
-            $show[$space] = $name;
+            show[space] = name;
 
-            for (var $ix = 0; $ix < $tabs[$space].length; $ix++) {
-                $element = document.querySelector(
-                    '[fx-tab-fill="' + $space + '.' + $tabs[$space][$ix] + '"]'
+            for (var i = 0; i < tabs[space].length; i++) {
+                element = document.querySelector(
+                    '[fx-tab-fill="' + space + '.' + tabs[space][i] + '"]'
                 );
 
-                if (typeof $element !== 'undefined') {
-                    angular.element($element).hide();
+                if (typeof element !== 'undefined') {
+                    angular.element(element).hide();
                 }
             }
 
-            $element = document.querySelector(
-                '[fx-tab-fill="' + $space + '.' + $name + '"]'
+            element = document.querySelector(
+                '[fx-tab-fill="' + space + '.' + name + '"]'
             );
 
-            if (typeof $element !== 'undefined') {
-                angular.element($element).show();
-            }
+            if (typeof element !== 'undefined')
+                angular.element(
+                    element
+                ).show();
         };
 
-        angular.element($window).bind('load', function() {
-            for (var $space in $tabs) {
-                if ($tabs[$space].indexOf($hash) !== -1) {
-                    var $element = document.querySelector(
-                        '[fx-tab="' + $space + '.' + $hash + '"]'
+        angular.element(window).ready(function() {
+            // console.log(
+            //     document.querySelectorAll('[fx-tab]')
+            // );
+
+            for (var space in tabs) {
+                if (tabs[space].indexOf(hash) !== -1) {
+                    var element = document.querySelector(
+                        '[fx-tab="' + space + '.' + hash + '"]'
                     );
 
-                    angular.element($element).click();
+                    angular.element(element).click();
 
 
                 } else {
-                    var $element = document.querySelector(
-                        '[fx-tab="' + $space + '.' + $tabs[$space][0] + '"]'
+                    var element = document.querySelector(
+                        '[fx-tab="' + space + '.' + tabs[space][0] + '"]'
                     );
 
-                    angular.element($element).click();
+                    angular.element(element).click();
                 }
             }
        });
@@ -65,15 +70,15 @@
         return {
             restrict: 'A',
             link: function($scope, $element, $attrs) {
-                var $space, $name;
+                var space, name;
 
                 if ($attrs.fxTab && /^(.*?)\.(.*?)$/i.test($attrs.fxTab) === true) {
-                    [$space, $name] = $attrs.fxTab.split('.');
+                    [space, name] = $attrs.fxTab.split('.');
 
-                    $tab.add($space, $name);
+                    $tab.add(space, name);
 
                     $element.bind('click', function() {
-                        $tab.toggle($space, $name);
+                        $tab.toggle(space, name);
                     });
                 }
             }
